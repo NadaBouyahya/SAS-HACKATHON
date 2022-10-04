2ffa32e7317aafe4b1ab344ba5b95cfbe031ca48
Merradou Abderrahmane <abderahmane.merradou@gmail.com>
2022-10-04 16:48:59 +0100

    Prototype basic

---
+++ prototype basic/app.js
@@ -0,0 +1,116 @@
+// Array Of Words
+const words = [
+    "Hello",
+    "World",
+    "typing",
+    "game",
+    "Programming",
+    "Code",
+    "Javascript",
+    "Github",
+    "Coding",
+    "Funny",
+    "Working",
+    "Task",
+    "Test",
+  ];
+  
+  // Setting Levels
+  const lvls = {
+    "Easy": 5,
+    "Normal": 3,
+    "Hard": 2
+  };
+  
+  // Default Level
+  let defaultLevelName = "Normal"; // Change Level From Here
+  let defaultLevelSeconds = lvls[defaultLevelName];
+  
+  // Catch Selectors
+  let startButton = document.querySelector(".start");
+  let lvlNameSpan = document.querySelector(".message .lvl");
+  let secondsSpan = document.querySelector(".message .seconds");
+  let theWord = document.querySelector(".the-word");
+  let upcomingWords = document.querySelector(".upcoming-words");
+  let input = document.querySelector(".input");
+  let timeLeftSpan = document.querySelector(".time span");
+  let scoreGot = document.querySelector(".score .got");
+  let scoreTotal = document.querySelector(".score .total");
+  let finishMessage = document.querySelector(".finish");
+  
+  // Setting Level Name + Seconds + Score
+  lvlNameSpan.innerHTML = defaultLevelName;
+  secondsSpan.innerHTML = defaultLevelSeconds;
+  timeLeftSpan.innerHTML = defaultLevelSeconds;
+  scoreTotal.innerHTML = words.length;
+  
+  // Disable Paste Event
+  input.onpaste = function () {
+    return false;
+  }
+  
+  // Start Game
+  startButton.onclick = function () {
+    this.remove();
+    input.focus();
+    // Generate Word Function
+    genWords();
+  }
+  
+  function genWords() {
+    // Get Random Word From Array
+    let randomWord = words[Math.floor(Math.random() * words.length)];
+    // Get Word Index
+    let wordIndex = words.indexOf(randomWord);
+    // Remove WordFrom Array
+    words.splice(wordIndex, 1);
+    // Show The Random Word
+    theWord.innerHTML = randomWord;
+    // Empty Upcoming Words
+    upcomingWords.innerHTML = '';
+    // Generate Words
+    for (let i = 0; i < words.length; i++) {
+      // Create Div Element
+      let div = document.createElement("div");
+      let txt = document.createTextNode(words[i]);
+      div.appendChild(txt);
+      upcomingWords.appendChild(div);
+    }
+    // Call Start Play Function
+    startPlay();
+  }
+  
+  function startPlay() {
+    let start = setInterval(() => {
+      timeLeftSpan.innerHTML--;
+ {
+        // Stop Timer
+        clearInterval(start);
+        // Compare Words
+        if (theWord.innerHTML.toLowerCase() === input.value.toLowerCase()) {
+          // Empty Input Field
+          input.value = '';
+          // Increase Score
+          scoreGot.innerHTML++;
+          if (words.length > 0) {
+            // Call Generate Word Function
+            genWords();
+          } else {
+            let span = document.createElement("span");
+            span.className = 'good';
+            let spanText = document.createTextNode("Congratulations, You Got All Words");
+            span.appendChild(spanText);
+            finishMessage.appendChild(span);
+            // Remove Upcoming Words Box
+            upcomingWords.remove();
+          }
+        } else {
+          let span = document.createElement("span");
+          span.className = 'bad';
+          let spanText = document.createTextNode("Game Over");
+          span.appendChild(spanText);
+          finishMessage.appendChild(span);
+        }
+      }
+    }, 1000);
+  }

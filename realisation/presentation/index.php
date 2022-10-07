c75d3362d1acf2cc645428c8a484e00744d905f8
Merradou-Abderrahmane <92019711+Merradou-Abderrahmane@users.noreply.github.com>
2022-10-07 07:01:21 +0100

    organize added files

--- realisation/presentation/index.php
+++ realisation/presentation/index.php
@@ -1,14 +1,29 @@
+<?php
+
+require_once "../buisness/playerBLL.php";
+    
+    $playerBal = new PlayerBLL();
+
+if(!empty($_POST)){
+	$player = new player();
+	$player->setUserName($_POST['username']);
+
+	$playerBal->addPlayer($player);
+	
+	// redirect to index.php
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Responsive Website Landing Page Design</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
   </head>
   <body>
-
     <section>
       <header>
         <h2><a href="#" class="logo">Logo</a></h2>
@@ -22,8 +37,10 @@
         <div class="info">
           <h2>Fast Typing <br><span>Type like a pro!</span></h2>
           <p> Typing well on the keyboard is very important. With "Fast Typing", you learn how to position your fingers to type like a pro, effortlessly. You learn with a fun game how to improve your speed and accuracy</p>
-          <input class="inputBox" placeholder="  Enter a username" type="text">
-          <a href="#" class="info-btn">Let's Start</a>
+          <form action="" method="POST">
+          <input type="text" name="username" class="inputBox" placeholder="  Enter a username" >
+          <button href="#" class="info-btn" type="submit">Let's Start</button>
+          </form>
         </div>
       </div>
       <div class="media-icons">
